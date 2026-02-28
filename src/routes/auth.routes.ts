faea52b3cd0e9e9b228225f3d1bd7f30af810af5
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-02-28 12:41:16 +0600

    auth route added

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { signup, login } from '../controllers/auth.controller';
+import { validate } from '../middlewares/validate.middleware';
+import { signupSchema, loginSchema } from '../validations/auth.validation';
+
+const router = Router();
+
+router.post('/signup', validate(signupSchema), signup);
+router.post('/login', validate(loginSchema), login);
+
+export default router;
