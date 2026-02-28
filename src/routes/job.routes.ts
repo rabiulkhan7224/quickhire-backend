4ac0bd41303f934c4a056ebfee3d7f90c8a8a4fd
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2026-02-28 12:41:38 +0600

    jobs route added

---
+++ src/routes/job.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import {
+  getAllJobs,
+  getJobById,
+  createJob,
+  deleteJob,
+} from '../controllers/job.controller';
+import { validate } from '../middlewares/validate.middleware';
+import { createJobSchema } from '../validations/job.validation';
+import { authMiddleware } from '../middlewares/auth.middleware';
+import { adminOnly } from '../middlewares/role.middleware';
+
+const router = Router();
+
+router.get('/', getAllJobs);
+router.get('/:id', getJobById);
+
+// Admin only
+router.post('/', authMiddleware, adminOnly, validate(createJobSchema), createJob);
+router.delete('/:id', authMiddleware, adminOnly, deleteJob);
+
+export default router;
